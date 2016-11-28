d375cacf068c58d2b87d300ea2ea5f664a44ce79
ghtpdkzz <ghtpdkzz@naver.com>
2016-11-28 18:09:54 +0900

    프레임 구성

---
+++ app/src/main/java/com/example/leemoonseong/scheduler/GridAdapter.java
@@ -0,0 +1,8 @@
+package com.example.leemoonseong.scheduler;
+
+/**
+ * Created by Hosea on 2016-11-28.
+ */
+
+public class GridAdapter {
+}
---
+++ app/src/main/java/com/example/leemoonseong/scheduler/MonthlyFragment.java
@@ -0,0 +1,24 @@
+package com.example.leemoonseong.scheduler;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Hosea on 2016-11-28.
+ */
+public class MonthlyFragment extends Fragment {
+
+    public MonthlyFragment(){}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_monthly, container, false);
+    }
+
+
+}
