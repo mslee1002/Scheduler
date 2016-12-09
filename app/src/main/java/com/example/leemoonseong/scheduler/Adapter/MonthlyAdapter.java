7acf2d37eb484dec6e4bc270d313045cdbd07978
mslee1002 <leemsmail@naver.com>
2016-12-09 15:05:06 +0900

    월별 보기 구현

---
+++ app/src/main/java/com/example/leemoonseong/scheduler/Adapter/MonthlyAdapter.java
@@ -0,0 +1,73 @@
+package com.example.leemoonseong.scheduler.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.leemoonseong.scheduler.R;
+import com.example.leemoonseong.scheduler.dao.ScheduleVO;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
+/**
+ * Created by LeeMoonSeong on 2016-12-09.
+ */
+
+public class MonthlyAdapter extends BaseAdapter {
+    private Context dContext;
+    private int dResource;
+    private ArrayList<ScheduleVO> dItems = new ArrayList<ScheduleVO>();
+
+    public MonthlyAdapter(Context context, int resource, ArrayList<ScheduleVO> items) {
+        dContext = context;
+        dResource = resource;
+        dItems = items;
+    }
+
+    @Override
+    public int getCount() {
+        return dItems.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return dItems.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        if (view == null) {
+            LayoutInflater inflater =
+                    (LayoutInflater) dContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(dResource, viewGroup,false);
+        }
+
+        // Set DayName
+        TextView startTime = (TextView) view.findViewById(R.id.tv_day_startTime);
+        TextView endTime = (TextView) view.findViewById(R.id.tv_day_endTime);
+        TextView title = (TextView) view.findViewById(R.id.tv_day_title);
+        TextView location= (TextView) view.findViewById(R.id.tv_day_location);
+//        TextView scheduleTime= (TextView) view.findViewById(R.id.tv_week_time);
+
+
+        // Set Text 01
+        ScheduleVO scheduleVO = dItems.get(i);
+        SimpleDateFormat formatForTime = new SimpleDateFormat("HH:mm");
+        startTime.setText(formatForTime.format(scheduleVO.getStartTime()));
+        endTime.setText(formatForTime.format(scheduleVO.getEndTime()));
+        title.setText(scheduleVO.getTitle());
+        location.setText(scheduleVO.getLocation());
+
+        return view;
+    }
+
+}
--- app/src/main/java/com/example/leemoonseong/scheduler/fragment/MonthlyFragment.java
+++ app/src/main/java/com/example/leemoonseong/scheduler/fragment/MonthlyFragment.java
@@ -1,6 +1,8 @@
 package com.example.leemoonseong.scheduler.fragment;
 
 import android.app.Fragment;
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,13 +10,20 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.GridView;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.leemoonseong.scheduler.Activity.ScheduleDetailActivity;
+import com.example.leemoonseong.scheduler.Adapter.DailyAdapter;
 import com.example.leemoonseong.scheduler.Adapter.GridAdapter;
 import com.example.leemoonseong.scheduler.Activity.MainActivity;
+import com.example.leemoonseong.scheduler.Adapter.MonthlyAdapter;
+import com.example.leemoonseong.scheduler.Database.MyDBHelper;
 import com.example.leemoonseong.scheduler.R;
+import com.example.leemoonseong.scheduler.dao.ScheduleVO;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -27,23 +36,34 @@ import java.util.Locale;
 public class MonthlyFragment extends Fragment {
 
     private ArrayList<String> dayList;
+    private ArrayList<ScheduleVO> dayListForMonth;
     //동적으로 변화하는 달력을 위해 현재 연,월을 저장힉 위한 변수
-
+    int db_year, db_month, db_day;
     static Calendar mCal;
     private int Year, Month;
     private TextView tvDate;
     GridAdapter gridAdapter;
+    MyDBHelper helper;
     int dayNum;
     int real_day;
     //오늘 날짜를 가지는 변수 (동적으로 변하는 변수가 아님)
     private int original_month;
-
+    MonthlyAdapter monthlyAdapter;
     public MonthlyFragment(){}
 
     @Override
     public void onResume(){
         super.onResume();
         ((MainActivity) getActivity()).setActionBarTitle("월별 보기");
+        if (monthlyAdapter != null) {
+            try {
+                load_dailySchedule();
+            } catch (ParseException e) {
+                e.printStackTrace();
+            } finally {
+                monthlyAdapter.notifyDataSetChanged();
+            }
+        }
         gridAdapter.notifyDataSetChanged();
     }
 
@@ -55,7 +75,7 @@ public class MonthlyFragment extends Fragment {
         final View view = inflater.inflate(R.layout.fragment_monthly, container, false);
         tvDate = (TextView)view.findViewById(R.id.tv_month_date);
         final GridView gridView = (GridView)view.findViewById(R.id.gridview);
-
+        final ListView listView = (ListView) view.findViewById(R.id.lv_monthly);
 
         // 오늘에 날짜를 세팅 해준다.
 
@@ -67,7 +87,7 @@ public class MonthlyFragment extends Fragment {
         final SimpleDateFormat curYearFormat = new SimpleDateFormat("yyyy", Locale.KOREA);
         final SimpleDateFormat curMonthFormat = new SimpleDateFormat("MM", Locale.KOREA);
         final SimpleDateFormat curDayFormat = new SimpleDateFormat("dd", Locale.KOREA);
-
+        helper = new MyDBHelper(view.getContext());
         //현재 날짜 텍스트뷰에 뿌려줌
         Year = Integer.parseInt(curYearFormat.format(date));
         original_month =Integer.parseInt(curMonthFormat.format(date));
@@ -176,6 +196,25 @@ public class MonthlyFragment extends Fragment {
                                     int position, long id) {
                 real_day = position -5 -dayNum;
                 Toast.makeText(view.getContext(),Year+"/"+Month+"/"+real_day,Toast.LENGTH_SHORT).show();
+                try {
+                    load_dailySchedule();
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        dayListForMonth = new ArrayList<ScheduleVO>();
+        monthlyAdapter = new MonthlyAdapter(view.getContext(),R.layout.monthlyitem, dayListForMonth);
+        listView.setAdapter(monthlyAdapter); // uses the view to get the context instead of getActivity().
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                real_day = position -5 -dayNum;
+                Intent intent = new Intent(getActivity(), ScheduleDetailActivity.class);
+                intent.putExtra("scheduleId", dayListForMonth.get(position).getScheduleId());
+                startActivity(intent);
             }
         });
 
@@ -193,8 +232,36 @@ public class MonthlyFragment extends Fragment {
             dayList.add("" + (i + 1));
         }
     }
+    public void load_dailySchedule() throws ParseException {
+        dayListForMonth.clear();
+        String sql = "Select * FROM schedule ";
+        Cursor cursor = helper.getReadableDatabase().rawQuery(sql,null);
+        StringBuffer buffer = new StringBuffer();
+        final SimpleDateFormat curYearFormat = new SimpleDateFormat("yyyy", Locale.KOREA);
+        final SimpleDateFormat curMonthFormat = new SimpleDateFormat("MM", Locale.KOREA);
+        final SimpleDateFormat curDayFormat = new SimpleDateFormat("dd", Locale.KOREA);
 
+        while (cursor.moveToNext()) {
+//            1. title, 2. startTime, 3 .endTime ,4. location, 5. memo,6. image
+//            Calendar t = new GregorianCalendar();
+            SimpleDateFormat dateFormat = new  SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.KOREA);
+            db_year = Integer.parseInt(curYearFormat.format(dateFormat.parse(cursor.getString(2))));
+            db_month = Integer.parseInt(curMonthFormat.format(dateFormat.parse(cursor.getString(2))));
+            db_day = Integer.parseInt(curDayFormat.format(dateFormat.parse(cursor.getString(2))));
+            String check = db_year+"-"+db_month+"-"+db_day;
+            if(check.equals(Year+"-"+Month+"-"+real_day)) {
 
-
-
+//            Date s_time = dateFormat.parse(cursor.getString(2)); //replace 4 with the column index
+//            Date e_time = dateFormat.parse(cursor.getString(3)); //replace 4 with the column index
+                dayListForMonth.add(new ScheduleVO(cursor.getInt(0), cursor.getString(1),
+                        dateFormat.parse(cursor.getString(2)), dateFormat.parse(cursor.getString(3)),
+                        cursor.getString(4), cursor.getString(5), cursor.getString(6)));
+                monthlyAdapter.notifyDataSetChanged();
+            }
+            else{
+                dayListForMonth.clear();
+                monthlyAdapter.notifyDataSetChanged();
+            }
+        }
+    }
 }
