fd82175d424c753edadf6c5c8571b361814e1431
이호세아 <ghtpdkzz@naver.com>
2016-12-09 19:07:38 +0900

    weekly 수정

--- app/src/main/java/com/example/leemoonseong/scheduler/Activity/MainActivity.java
+++ app/src/main/java/com/example/leemoonseong/scheduler/Activity/MainActivity.java
@@ -9,6 +9,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.database.Cursor;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
@@ -19,14 +20,24 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import com.example.leemoonseong.scheduler.Database.MyDBHelper;
 import com.example.leemoonseong.scheduler.R;
+import com.example.leemoonseong.scheduler.dao.ScheduleVO;
 import com.example.leemoonseong.scheduler.fragment.DailyFragment;
 import com.example.leemoonseong.scheduler.fragment.MonthlyFragment;
 import com.example.leemoonseong.scheduler.fragment.WeeklyFragment;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
+    private ArrayList<ScheduleVO> dayList;
     private String currentFragment = "monthly";
-    ActionBar ab;
+    private ActionBar ab;
+    private MyDBHelper helper;
+    private String today, schedule;
     final MonthlyFragment monthlyFragment = new MonthlyFragment();
     final WeeklyFragment weeklyFragment = new WeeklyFragment();
     final DailyFragment dailyFragment = new DailyFragment();
@@ -41,8 +52,33 @@ public class MainActivity extends AppCompatActivity {
         ab = getSupportActionBar();
         ab.setTitle("월별 보기");
         setContentView(R.layout.activity_main);
+        helper = new MyDBHelper(getApplicationContext());
+        dayList = new ArrayList<>();
+        SimpleDateFormat sfm = new SimpleDateFormat("yyyy-MM-dd");
+        today = sfm.format(new Date());
     }
 
+    @Override
+    protected void onResume(){
+        super.onResume();
+        String sql = "Select * FROM schedule;";
+        StringBuffer sb = new StringBuffer();
+        Cursor cursor = helper.getReadableDatabase().rawQuery(sql,null);
+        while (cursor.moveToNext()) {
+//            1. title, 2. startTime, 3 .endTime ,4. location, 5. memo,6. image
+//            Calendar t = new GregorianCalendar();
+            SimpleDateFormat dateFormat = new  SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.KOREA);
+            if (cursor.getString(2).startsWith(today))
+            {
+                sb.append(" " + cursor.getString(2));
+            }
+        }
+        if (sb.toString().equals("")) {
+            NotificationSomethings("없음");
+        } else {
+            NotificationSomethings(sb.toString());
+        }
+    }
 
 
     protected void switchFragment(int id) {
@@ -112,5 +148,32 @@ public class MainActivity extends AppCompatActivity {
             super.onBackPressed();
     }
 
+    public void NotificationSomethings(String msg) {
+        Resources res = getResources();
+
+        Intent notificationIntent = new Intent(this, NotificationSomething.class);
+        notificationIntent.putExtra("notificationId", 9999); //전달할 값
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+
+        builder.setContentTitle("이문성 이호세아의 scheduler")
+                .setContentText("오늘의 스케줄 :" + msg)
+                .setTicker("")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setLargeIcon(BitmapFactory.decodeResource(res, R.mipmap.ic_launcher))
+                .setContentIntent(contentIntent)
+                .setAutoCancel(true)
+                .setWhen(System.currentTimeMillis())
+                .setDefaults(Notification.DEFAULT_ALL);
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            builder.setCategory(Notification.CATEGORY_MESSAGE)
+                    .setPriority(Notification.PRIORITY_HIGH)
+                    .setVisibility(Notification.VISIBILITY_PUBLIC);
+        }
+        NotificationManager nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        nm.notify(1234, builder.build());
+    }
 
 }
